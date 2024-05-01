ce75837af80622308edcef05ab36a1cc5c7379cd
Mostakimw <scientist.expert@gmail.com>
2024-05-01 11:34:48 +0600

    semester creation done, error handled

--- src/pages/admin/AcademicManagement/AcademicSemester.tsx
+++ src/pages/admin/AcademicManagement/AcademicSemester.tsx
@@ -1,8 +1,7 @@
-import { useGetAllSemestersQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
+import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.api";
 
 const AcademicSemester = () => {
   const { data } = useGetAllSemestersQuery(undefined);
-  console.log(data);
   return <div>this is academic semester</div>;
 };
 
--- src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
@@ -6,6 +6,9 @@ import { monthOptions } from "../../../constants/global";
 import { semestersName } from "../../../constants/semester";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { academicSemesterSchema } from "../../../schemas/academicManagement/academicManagement.schema";
+import { useAddAcademicSemesterMutation } from "../../../redux/features/admin/academicManagement.api";
+import { toast } from "sonner";
+import { TResponse } from "../../../types/global";
 
 // ! year options
 const currentYear = new Date().getFullYear();
@@ -16,7 +19,10 @@ const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
 }));
 
 const CreateAcademicSemester = () => {
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+  const [addAcademicSemester] = useAddAcademicSemesterMutation();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading("Creating...");
     const name = semestersName[Number(data.name) - 1].label;
     const createSemData = {
       name,
@@ -25,7 +31,20 @@ const CreateAcademicSemester = () => {
       startMonth: data?.startMonth,
       endMonth: data?.endMonth,
     };
-    console.log(createSemData);
+    try {
+      console.log(createSemData);
+      const res = await addAcademicSemester(createSemData) as TResponse;
+      console.log(res);
+
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        addAcademicSemester(createSemData);
+        toast.success("Semester Created", { id: toastId });
+      }
+    } catch (err) {
+      toast.error("Something went wrong", { id: toastId });
+    }
   };
 
   return (
