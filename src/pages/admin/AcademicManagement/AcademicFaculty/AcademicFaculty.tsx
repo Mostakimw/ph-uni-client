ffe64e016a87d3ca1d7a393775db8d0db777e477
Mostakimw <scientist.expert@gmail.com>
2024-05-01 20:35:54 +0600

    academic faculty completed

--- src/pages/admin/AcademicManagement/AcademicFaculty/AcademicFaculty.tsx
+++ src/pages/admin/AcademicManagement/AcademicFaculty/AcademicFaculty.tsx
@@ -1,12 +1,43 @@
+import { Col, Flex, Table, TableColumnsType, TableProps } from "antd";
 import { useGetAllAcademicFacultyQuery } from "../../../../redux/features/admin/academicManagement.api";
 
+type TAcademicFaculty = {
+  _id: string;
+  name: string;
+};
+
 const AcademicFaculty = () => {
-  const { data: academicFaculty } = useGetAllAcademicFacultyQuery(undefined);
-  console.log(academicFaculty);
+  const { data: academicFaculty, isFetching } =
+    useGetAllAcademicFacultyQuery(undefined);
+
+  const tableData = academicFaculty?.data?.map(
+    ({ _id, name }: TAcademicFaculty) => ({
+      key: _id,
+      name,
+    })
+  );
+
+  const columns: TableColumnsType<TAcademicFaculty> = [
+    {
+      title: "Faculty Name",
+      dataIndex: "name",
+    },
+  ];
+
+  const onChange: TableProps<TAcademicFaculty>["onChange"] = () => {
+    console.log("onChange");
+  };
   return (
-    <div>
-      <h1>This is AcademicFaculty component</h1>
-    </div>
+    <Flex justify="center">
+      <Col span={6}>
+        <Table
+          loading={isFetching}
+          dataSource={tableData}
+          columns={columns}
+          onChange={onChange}
+        ></Table>
+      </Col>
+    </Flex>
   );
 };
 
